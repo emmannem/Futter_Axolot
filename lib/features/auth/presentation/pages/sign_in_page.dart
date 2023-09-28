ffd4980fa872ad9fd6a9f3939a81637596a83181
Emmanuel Rivera <129634455+emmannem@users.noreply.github.com>
2023-09-28 15:36:11 -0600

    Login Closes #1  (#10)
    * Modified Login #1
    
    * Update Login #1

--- lib/features/auth/presentation/pages/sign_in_page.dart
+++ lib/features/auth/presentation/pages/sign_in_page.dart
@@ -1,17 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:ui_one/features/auth/presentation/pages/admin_page.dart';
-import 'package:ui_one/features/auth/presentation/pages/main_home.dart';
-
-import 'package:ui_one/features/auth/presentation/pages/app_widget.dart';
+import 'package:ui_one/features/auth/presentation/pages/sign_up_page.dart';
 import 'package:ui_one/features/auth/presentation/validator/auth_validator.dart';
-import 'package:ui_one/service._locator.dart';
-
-import '../components/buttons.dart';
 
 class SignInPage extends StatefulWidget {
   static const String id = "sign_in_page";
 
-  const SignInPage({super.key});
+  const SignInPage({Key? key}) : super(key: key);
 
   @override
   State<SignInPage> createState() => _SignInPageState();
@@ -27,96 +21,115 @@ class _SignInPageState extends State<SignInPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          children: [
-            const SizedBox(
-              width: 100,
-              height: 10,
-            ),
-            const Center(
-              child: Text(
-                "Welcome back!\nSign in to continue!",
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Iniciar sesión",
                 textAlign: TextAlign.center,
                 style: TextStyle(
-                  fontSize: 30,
+                  fontSize: 36,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFF6739FF),
                 ),
               ),
-            ),
-            const SizedBox(
-              width: 100,
-              height: 10,
-            ),
-            Column(
-              children: const [
-                MyButton(
-                  iconUrl: 'assets/images/ic_google.png',
-                  text: "Log in with Google",
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                "¡Hola! Bienvenido de nuevo, te hemos extrañado",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.grey,
                 ),
-                SizedBox(height: 20),
-                MyButton(
-                  iconUrl: 'assets/images/ic_facebook.png',
-                  text: "Log in with Google",
-                ),
-                SizedBox(height: 20),
-                Text(
-                  "Or",
-                  style: TextStyle(
-                    color: Colors.grey,
-                  ),
-                ),
-              ],
-            ),
-            Form(
-              key: _signInGlobalKey,
-              child: Column(
-                children: [
-                  TextFormField(
-                    controller: emailController,
-                    validator: AuthValidator.isEmailValid,
-                    decoration:
-                        const InputDecoration(hintText: "email address"),
-                  ),
-                  const SizedBox(height: 30),
-                  TextFormField(
-                    controller: passwordController,
-                    obscureText: passwordSee,
-                    validator: AuthValidator.isPasswordValid,
-                    decoration: InputDecoration(
-                      hintText: "password",
-                      suffixIcon: GestureDetector(
-                        onTap: () {
-                          passwordSee = !passwordSee;
-                          setState(() {});
-                        },
-                        child: Icon(
-                          passwordSee
-                              ? Icons.visibility_off_outlined
-                              : Icons.visibility_outlined,
+              ),
+              SizedBox(
+                height: 60,
+              ),
+              Form(
+                key: _signInGlobalKey,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      controller: emailController,
+                      validator: AuthValidator.isEmailValid,
+                      decoration: const InputDecoration(
+                        hintText: "Correo",
+                      ),
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    TextFormField(
+                      controller: passwordController,
+                      obscureText: passwordSee,
+                      validator: AuthValidator.isPasswordValid,
+                      decoration: InputDecoration(
+                        hintText: "Contraseña",
+                        suffixIcon: GestureDetector(
+                          onTap: () {
+                            passwordSee = !passwordSee;
+                            setState(() {});
+                          },
+                          child: Icon(
+                            passwordSee
+                                ? Icons.visibility_off_outlined
+                                : Icons.visibility_outlined,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            const SizedBox(height: 100),
-            Column(
-              children: [
-                MyButtonTwo(text: "Log in", onPressed: signIn),
-                const SizedBox(height: 30),
-                const Text(
-                  "Forgot Password",
+              SizedBox(
+                height: 80,
+              ),
+              ElevatedButton(
+                onPressed: signIn,
+                child: Text(
+                  "Ingresar",
                   style: TextStyle(
-                    fontWeight: FontWeight.w500,
                     fontSize: 18,
-                    color: Color(0xFF265AE8),
                   ),
                 ),
-              ],
-            ),
-          ],
+                style: ElevatedButton.styleFrom(
+                  primary: Color(0xFF6739FF),
+                  padding: EdgeInsets.symmetric(horizontal: 125, vertical: 22),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                "¿No tienes una cuenta?",
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.black, // Color negro
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  // Navegar a la página de registro cuando se presione
+                  Navigator.pushNamed(context, SignUpPage.id);
+                },
+                child: Text(
+                  "Registrarse",
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Color(0xFF6739FF), // Color personalizado
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -124,29 +137,7 @@ class _SignInPageState extends State<SignInPage> {
 
   void signIn() {
     if (_signInGlobalKey.currentState!.validate()) {
-      final message = authController.login(
-        emailController.text.trim(),
-        passwordController.text.trim(),
-      );
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text(message["message"] as String),
-          margin:
-              EdgeInsets.only(bottom: MediaQuery.of(context).size.height * .9),
-          behavior: SnackBarBehavior.floating,
-          duration: const Duration(seconds: 5),
-          shape: const StadiumBorder(),
-          dismissDirection: DismissDirection.horizontal,
-          showCloseIcon: true,
-        ),
-      );
-      if (message["next"] == "next") {
-        AppWidget.isLogin = true;
-        AppWidget.loggedUser["email"] = emailController.text.trim();
-        AppWidget.loggedUser["password"] = passwordController.text.trim();
-        //Navigator.pushNamed(context, AdminPage.id);
-        Navigator.pushNamed(context, MyApp.id);
-      }
+      // Agrega aquí tu lógica de inicio de sesión
     }
   }
 
