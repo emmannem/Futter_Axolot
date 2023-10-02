a873f79c8dba2cf2cea470cd426b4e7988caa1b0
Joseemmanuel <emmanuelriv15@gmail.com>
2023-10-02 12:55:54 -0600

    Update Login #1

---
+++ lib/service/auth_service.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+
+class AuthService with ChangeNotifier {
+  bool _auntenticando = false;
+
+  bool get autenticando => _auntenticando;
+  set autenticando(bool valor) {
+    _auntenticando = valor;
+    notifyListeners();
+  }
+
+  Future<String?> login(String username, String password) async {
+    final HttpLink httpLink = HttpLink(
+      "Link to your GraphQL API",
+    );
+
+    final GraphQLClient client = GraphQLClient(
+      cache: GraphQLCache(),
+      link: httpLink,
+    );
+
+    final MutationOptions options = MutationOptions(
+      document: gql('''
+    Your Mutation
+  '''),
+      variables: <String, dynamic>{
+        'username': username,
+        'password': password,
+      },
+    );
+
+    final QueryResult result = await client.mutate(options);
+
+    if (result.hasException) {
+      return null;
+    }
+
+    final String? token = result.data?['tokenAuth']['token'];
+
+    return token;
+  }
+}
